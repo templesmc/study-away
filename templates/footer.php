58edf5e60f56568797bc9155f4cf1bb74384f993
Chris Montgomery <chris@montchr.io>
2014-09-29 15:49:44 -0400

    display content last modified in footer
    see templesmc/smc-themes#74

--- templates/footer.php
+++ templates/footer.php
@@ -1,6 +1,7 @@
 <footer class="content-info" role="contentinfo">
   <div class="container">
     <?php dynamic_sidebar('sidebar-footer'); ?>
+    <p class="content-last-modified"><small>Content last updated <?php the_modified_time('F j, Y'); ?> at <?php the_modified_time('g:i a'); ?></small></p>
   </div>
 </footer>
 
