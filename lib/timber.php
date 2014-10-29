3fed43fe00b4129715e80dee88fd25504b30b2d9
Chris Montgomery <chris@montchr.io>
2014-10-29 14:49:09 -0400

    set up base.twig

--- lib/timber.php
+++ lib/timber.php
@@ -2,6 +2,8 @@
 
 function smc_filter_timber_context ( $context ) {
 
+  $context['get_header'] = do_action( 'header' );
+
   $context['site']['nav']                   = wp_nav_menu(array('theme_location' => 'primary_navigation', 'menu_class' => 'nav navbar-nav'));
   $context['site']['feed_link']             = esc_url(get_feed_link());
   $context['site']['browse_happy']          = __('You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.', 'smc');
