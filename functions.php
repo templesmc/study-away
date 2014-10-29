65f3cd0958fe8f172c8f8b29578c24768493ead9
Chris Montgomery <chris@montchr.io>
2014-10-29 14:31:21 -0400

    remove additional timber functions

--- functions.php
+++ functions.php
@@ -38,27 +38,3 @@
     require_once $filepath;
   }
   unset($file, $filepath);
-
-	add_filter('get_twig', 'add_to_twig');
-	add_filter('timber_context', 'add_to_context');
-
-
-	define('THEME_URL', get_template_directory_uri());
-	function add_to_context($data){
-		/* this is where you can add your own data to Timber's context object */
-		$data['qux'] = 'I am a value set in your functions.php file';
-		$data['menu'] = new TimberMenu();
-		return $data;
-	}
-
-	function add_to_twig($twig){
-		/* this is where you can add your own fuctions to twig */
-		$twig->addExtension(new Twig_Extension_StringLoader());
-		$twig->addFilter('myfoo', new Twig_Filter_Function('myfoo'));
-		return $twig;
-	}
-
-	function myfoo($text){
-    	$text .= ' bar!';
-    	return $text;
-	}
