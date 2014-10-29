12ab57d995b814ac7659513bf3488e39692d7a4d
Chris Montgomery <chris@montchr.io>
2014-10-29 15:13:52 -0400

    404 page

--- 404.php
+++ 404.php
@@ -10,4 +10,12 @@
  */
 
 	$context = Timber::get_context();
+
+  $context['msg']              = array();
+  $context['msg']['reasons']   = array();
+  $context['msg']['alert']     = __('Sorry, but the page you were trying to view does not exist.', 'smc');
+  $context['msg']['explain']   = __('It looks like this was the result of either:', 'smc');
+  $context['msg']['reasons'][] = __('a mistyped address', 'smc');
+  $context['msg']['reasons'][] = __('an out-of-date link', 'smc');
+
 	Timber::render('404.twig', $context);
