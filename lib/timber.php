97b4cf7c4765914df3b33c63c1cd2556ca886ecb
Chris Montgomery <chris@montchr.io>
2014-10-30 13:49:07 -0400

    use sitewide context properly
    Disable nav temporarily

--- lib/timber.php
+++ lib/timber.php
@@ -4,14 +4,18 @@ function smc_filter_timber_context ( $context ) {
 
   $context['get_header'] = do_action( 'header' );
 
-  $context['site']['nav']                   = wp_nav_menu(array('theme_location' => 'primary_navigation', 'menu_class' => 'nav navbar-nav'));
-  $context['site']['feed_link']             = esc_url(get_feed_link());
-  $context['site']['browse_happy']          = __('You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.', 'smc');
-  $context['site']['last_modified']['date'] = the_modified_time('F j, Y');
-  $context['site']['last_modified']['time'] = the_modified_time('g:i a');
+  // $context['site']['nav']                   = wp_nav_menu(array('theme_location' => 'primary_navigation', 'menu_class' => 'nav navbar-nav'));
+  $context['feed_link']             = esc_url(get_feed_link());
+  $context['browse_happy']          = __('You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.', 'smc');
+  $context['last_modified']['date'] = the_modified_time('F j, Y');
+  $context['last_modified']['time'] = the_modified_time('g:i a');
 
-  $context['post']['msg']['by']         = __('By', 'smc');
-  $context['post']['msg']['search_for'] = __('Search for:', 'smc');
+
+  $context['msg'] = array();
+  $context['msg']['by']         = __('By', 'smc');
+  $context['msg']['search'] = __('Search', 'smc');
+  $context['msg']['search_for'] = __('Search for:', 'smc');
+  $context['msg']['not_found'] = __('Sorry, no results were found.', 'smc');
 
   return $context;
 
