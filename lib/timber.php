976076b34f390f5114be504b490d8b5588d1a99a
Chris Montgomery <chris@montchr.io>
2014-11-04 13:39:41 -0500

    get the modified time, not echo

--- lib/timber.php
+++ lib/timber.php
@@ -7,8 +7,8 @@ function smc_filter_timber_context ( $context ) {
   // $context['site']['nav']                   = wp_nav_menu(array('theme_location' => 'primary_navigation', 'menu_class' => 'nav navbar-nav'));
   $context['feed_link']             = esc_url(get_feed_link());
   $context['browse_happy']          = __('You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.', 'smc');
-  $context['last_modified']['date'] = the_modified_time('F j, Y');
-  $context['last_modified']['time'] = the_modified_time('g:i a');
+  $context['last_modified']['date'] = get_the_modified_time('F j, Y');
+  $context['last_modified']['time'] = get_the_modified_time('g:i a');
 
 
   $context['msg'] = array();
